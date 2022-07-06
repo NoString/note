58cc0bf7656c348be5252a11507d0415ee7d2173
mrzhssss <pro6@qq.com>
2022-07-06 09:08:38 +0800

    #

---
+++ 前端/vue/vueProject/2.脚手架项目/4.todoList/src/components/Item.vue
@@ -0,0 +1,72 @@
+<template lang="html">
+    <div>
+        <li>
+          <label>
+            <input type="checkbox" :checked="obj.done" @change="handleChange(obj.id)"/>
+            <span>{{obj.content}}</span>
+          </label>
+          <button class="btn btn-danger" @click="doDelete(obj.id)">删除</button>
+        </li>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Item',
+    props:['obj', 'changeData', 'deleteItem'],
+    methods:{
+      handleChange(id){
+        this.changeData(id)
+      },
+      doDelete(id){
+        if(confirm('确定要删除吗?')){
+          this.deleteItem(id)
+        }
+      }
+    }
+}
+</script>
+<style lang="css" scoped>
+    /*item*/
+li {
+  list-style: none;
+  height: 36px;
+  line-height: 36px;
+  padding: 0 5px;
+  border-bottom: 1px solid #ddd;
+}
+
+li label {
+  float: left;
+  cursor: pointer;
+}
+
+li label li input {
+  vertical-align: middle;
+  margin-right: 6px;
+  position: relative;
+  top: -1px;
+}
+
+li button {
+  float: right;
+  display: none;
+  margin-top: 3px;
+}
+
+li:before {
+  content: initial;
+}
+
+li:last-child {
+  border-bottom: none;
+}
+
+li:hover{
+  background-color: #ddd;
+}
+
+li:hover button{
+  display: block;
+}
+
+</style>
--- 前端/vue/vueProject/2.脚手架项目/4.todoList/src/components/List.vue
+++ 前端/vue/vueProject/2.脚手架项目/4.todoList/src/components/List.vue
@@ -1,15 +1,34 @@
 <template lang="html">
     <div>
-
+        <ul class="todo-main">
+        <Item v-for="todo in todos" :key="todo.id" :obj="todo" :changeData="changeData" :deleteItem="deleteItem"/>
+      </ul>
     </div>
 </template>
 <script>
  import Item from "./Item";
 export default {
     name:'List',
-    comments:{ Item }
+    components:{Item},
+    props:['todos','changeData','deleteItem']
 }
 </script>
-<style lang="css">
+<style lang="css" scoped>
     
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
 </style>
